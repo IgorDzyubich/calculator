cc4d12663b3d0943bc4d6a25f417ee052fb9295f
igor_dzyubich <igor.dzyubich@gmail.com>
2020-06-06 14:43:51 +0300

    add primitive calculator example

---
+++ js/index.js
@@ -0,0 +1,48 @@
+let buttonPlus = document.getElementById("buttonPlus");
+let buttonMinus = document.getElementById("buttonMinus");
+let buttonMultiply = document.getElementById("buttonMultiply");
+let buttonDevide = document.getElementById("buttonDevide");
+
+let input1 = document.getElementById("number1");
+let input2 = document.getElementById("number2");
+
+function getNumber1() {
+    return Number(input1.value);
+}
+
+function getNumber2() {
+    return Number(input2.value);
+}
+
+function makeOperation(operationCode) {
+    if (operationCode === "+") {
+        var result = getNumber1() + getNumber2();
+    } else if (operationCode === "-") {
+        var result = getNumber1() - getNumber2();
+    } else if (operationCode === "*") {
+        var result = getNumber1() * getNumber2();
+    } else if (operationCode === "/") {
+        var result = getNumber1() / getNumber2();
+    } else {
+        window.alert("operation is unknown")
+    }
+    window.alert(result);
+}
+
+function onButtonPlusClick() { 
+    makeOperation("+");
+}
+function onButtonMinusClick() {   
+    makeOperation("-");
+}
+function onButtonMultiplyClick() {
+    makeOperation("*");
+}
+function onButtonDevideClick() {
+    makeOperation("/");
+}
+
+buttonPlus.addEventListener("click", onButtonPlusClick);
+buttonMinus.addEventListener("click", onButtonMinusClick);
+buttonMultiply.addEventListener("click", onButtonMultiplyClick);
+buttonDevide.addEventListener("click", onButtonDevideClick);
